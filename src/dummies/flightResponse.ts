5e72c35420462606ab5762dbc5c564206d1015bf
aytacguc <aytacguc@hotmail.com.tr>
2024-07-01 17:38:19 +0300

    fix

---
+++ src/dummies/flightResponse.ts
@@ -0,0 +1,11 @@
+import type { Flight } from '../interface/index'
+
+const response: Flight[] = [
+  { id: 1, name: 'Grand Plaza Hotel', stars: 4 },
+  { id: 2, name: 'Riverside Inn', stars: 5 },
+  { id: 3, name: 'Sunset Resort', stars: 3 },
+  { id: 4, name: 'Mountain View Lodge', stars: 4 },
+  { id: 5, name: 'Oceanfront Retreat', stars: 5 }
+]
+
+export default response
--- src/pages/FlightResults.vue
+++ src/pages/FlightResults.vue
@@ -3,11 +3,15 @@
 </template>
 
 <script lang="ts">
+import flightResponse from '../dummies/flightResponse'
 export default {
   data() {
     return {
       logo: 'https://cdn.dev.reisetech.io/airline_34x34/'
     }
+  },
+  created() {
+    console.log(flightResponse)
   }
 }
 </script>
