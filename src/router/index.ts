fed6f3768bb99307ec2087c2c7d180fa83959706
aytacguc <aytacguc@hotmail.com.tr>
2024-07-08 09:00:22 +0300

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../pages/HomeView.vue'
-
+import PinkPanthers from '../pages/PinkPanthers.vue'
 const router = createRouter({
   history: createWebHistory('./'),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/pinkpanthers',
+      name: 'pinkpanthers',
+      component: PinkPanthers
     }
   ]
 })
