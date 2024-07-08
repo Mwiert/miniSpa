c1dca3b20022fcc41e6b5dd9f5e67df92e2cb268
Defne Bulut <d.bulut0907@gmail.com>
2024-07-08 11:04:37 +0300

    Colors added, button component and page created, page adde to the router

---
+++ src/pages/PowerPuffGirls.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>Deneme</div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'PowerPuffGirls'
+}
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import PowerPuffGirls from '../pages/PowerPuffGirls.vue'
 const router = createRouter({
   history: createWebHistory('./'),
-  routes: []
+  routes: [
+    {
+      path: '/powerpuffgirls',
+      name: 'powerpuffgirls',
+      component: PowerPuffGirls
+    }
+  ]
 })
 
 export default router
