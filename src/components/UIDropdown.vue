52d1bedc5727a584e16123151e779236ef7f2d71
Nazli Gonca <nazligonca@Nazlis-MacBook-Air.local>
2024-07-08 11:27:57 +0300

    add page and component

---
+++ src/components/UIDropdown.vue
@@ -0,0 +1,28 @@
+<template>
+    <div >
+        denemedir
+    </div>
+  </template>
+  
+  <script>
+ 
+  
+  export default {
+    name: 'UIDropdown',
+    components: {
+    },
+    data() {
+    return {
+    }
+  },
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {}
+  
+  }
+  </script>
+  
+  <style>
+  /* Add your styles here */
+  </style> 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SummerLovers from '../pages/SummerLovers.vue'
 
 const router = createRouter({
   history: createWebHistory('./'),
-  routes: []
+  routes: [
+    {
+      path: '/summerlovers',
+      name: 'summerlovers',
+      component: SummerLovers
+    }
+  ]
 })
 
 export default router
