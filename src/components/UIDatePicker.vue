044a36d8fa789e0483584ee622d157e5dee83ccb
Emre Topcu <114030199+EmreTopcu04@users.noreply.github.com>
2024-07-08 15:23:38 +0300

    Style UIDatePicker

--- src/components/UIDatePicker.vue
+++ src/components/UIDatePicker.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="ui-date-picker-c">
-        <div class="date-holder">
-            {{ currentMonth + " " +currentYear }}
-        </div>
-        <div v-for= "(days,index) in weekdays" :key="index" class="days-wrapper">
-            <div class="day-names"> 
-               {{ weekdays[index] }}
+    <div class="ui-date-picker-c" >
+        <div class="ui-date-picker-wrapper">
+            <div class="calendar">
+                <div class="header">
+                    <button class="nav-button" @click="onClickToLeft">&lt;</button>
+                    <span class="current-date">{{ dateHolder }}</span>
+                    <button class="nav-button" @click="onClickToRight">&gt;</button>
+                </div>
             </div>
-            <div class= "days">
-                dfasfdas
+        
+            <div class="weekdays">
+                <span v-for="(weekdays,index) in weekdays" :key = index>
+                    {{ weekdays }}
+                </span>
+                <div class="days">
+                <span v-for="(days,index) in daysInMonth" :key="index">
+                    {{ days }}
+                </span>
             </div>
+            </div>
+        
         </div>
+
     </div>
-  </template>
+</template>
 
 
 <script lang="ts">
@@ -21,12 +32,49 @@ export default {
     name:"UIDatePicker",
     data() {
         return {
-            showCalendar: false,
-            currentMonth: dayjs().format('MMMM'),
-            currentYear: dayjs().format('YYYY'),
+            
             weekdays: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
             selectedDate: dayjs(),
             daysInMonth: []
+        }
+    },
+    methods: {
+        totalDaysInMonth(){
+            
+            let startOfMonth = this.selectedDate.startOf('month').format('D');
+            console.log(startOfMonth)
+            let endOfMonth = this.selectedDate.endOf('month').format('D');
+            console.log(endOfMonth)
+
+            for (let i = 0; i < endOfMonth; i++){
+                console.log(i)
+                this.daysInMonth.push(i)     
+            }
+        },
+
+        onClickToRight(){
+            this.selectedDate = this.selectedDate.add(1, 'month')
+
+        },
+
+        onClickToLeft(){
+            this.selectedDate = this.selectedDate.subtract(1, 'month')
+        },
+
+        currentMonth(){
+            return this.selectedDate.format('MMMM')
+        },
+        currentYear(){
+            return this.selectedDate.format('YYYY')
+        },
+    },
+    computed: {
+
+        dateHolder(){
+            return this.currentMonth() + " " + this.currentYear()
+        },
+        dateDaysHolder(){
+            return this.totalDaysInMonth()
         }
     }
 }
@@ -34,33 +82,76 @@ export default {
 
  
 <style lang="scss" scoped>
-@import '../assets/css/_fonts.scss';
+
+
+@import '../assets/css/variables.scss';
 
 .ui-date-picker-c{
+
+    position: absolute;
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-
-.date-holder{
+    width: 90%;
+    height: 90%;
     
-    font-family: Roboto;
-    letter-spacing: 0px;
-    color: #363636;
-    opacity: 1;
-    font-weight: bold;
+    .ui-date-picker-wrapper{
 
+        background: #FFFFFF;
+        box-shadow: 2px 2px 6px #5C75991A;
+        border: 1px solid #E6E6E6;
+        border-radius: $border-radius-medium;
+        opacity: 1;
+        width: 270px;
+        height: 240px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+
+        .calendar{
+            padding-top: 10px;
+            width: 200px;
+            .header{
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                .nav-button{
+                    background-color: transparent;
+                    border: none;
+                    font-size: 1.5rem;
+                    font-family: 'Arial';
+                    cursor: pointer;
+                }
+                .current-date{
+                    font-size: 1rem;
+                    font-weight: 500;
+                }
+            }
+        }
+        .weekdays{
+            padding-bottom: 120px;
+            width: 220px;
+            display: grid;
+            grid-template-columns: repeat(7, 1rem);
+            justify-content: space-between;
+            font-size: 14px;
+            font-weight: 700;
+            color: #363636;
+            opacity: 0.7;
+            .days{
+
+                width: 220px;
+                display: grid;
+                grid-template-columns: repeat(7, 1rem);
+                justify-content: space-between;
+                text-align: center
 }
-.days-wrapper{
-    .day-names{
-        display:flex;
-        justify-content: space-evenly;
-        flex-direction: row;
+        
+        }
+        
     }
-    .days{
-        display: grid;
-        justify-content: space-evenly;
-        padding: 10px;
-        font-size: 16px;
-    }
-}
+    
 }
 </style>
