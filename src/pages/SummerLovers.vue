5c03e26b9423d21ecb9119dd44fceb8c6aa29ab6
samet bilgin <sametbilgin@samet-MacBook-Air.local>
2024-07-08 12:24:14 +0300

    ...

--- src/pages/SummerLovers.vue
+++ src/pages/SummerLovers.vue
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> ce592a8b7598411f183fc84f2b2e3a3f2c005e6c
 <template>
     <div>
       <UIDropdown />
     </div>
   </template>
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> ce592a8b7598411f183fc84f2b2e3a3f2c005e6c
 
   <script lang="ts">
 
   import UIDropdown from "../components/UIDropdown.vue";
   export default {
-<<<<<<< HEAD
     components: {
       UIDropdown
       
@@ -27,14 +17,3 @@
   </script>
 
   <style lang="scss" scoped></style>
-=======
-    name: 'SummerLovers',
-    components: {
-      UIDropdown
-    },
-  }
-  </script>
-  
-  <style lang="scss" scoped></style>
-  
->>>>>>> ce592a8b7598411f183fc84f2b2e3a3f2c005e6c
