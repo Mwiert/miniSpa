13a955d982c89629b0a060481ef5523e8a344921
samet bilgin <sametbilgin@samet-MacBook-Air.local>
2024-07-08 12:21:09 +0300

    ..

--- src/components/UIDropdown.vue
+++ src/components/UIDropdown.vue
@@ -1,62 +1,36 @@
 <template>
-    <div >
-        denemedir
-    </div>
-  </template>
-<<<<<<< HEAD
+  <div >
+      denemedir
+  </div>
+</template>
 
-  <script>
- 
+<script>
 
-  export default {
-    name: 'UIDropdown',
 
-    components: {},
-    data() {
-      return {
-        
-      }
-    },
-    created() {
-      
-    },
-    mounted() {
-      
-    },
-    computed:{
+export default {
+  name: 'UIDropdown',
 
-    },
-    methods: {
-      
-    },
-  }
-  </script>
-
-  <style>
-  /* Add your styles here */
-  </style>
-=======
-  
-  <script>
- 
-  
-  export default {
-    name: 'UIDropdown',
-    components: {
-    },
-    data() {
+  components: {},
+  data() {
     return {
+      
     }
   },
-  created() {},
-  mounted() {},
-  computed: {},
-  methods: {}
-  
-  }
-  </script>
-  
-  <style>
-  /* Add your styles here */
-  </style> 
->>>>>>> ce592a8b7598411f183fc84f2b2e3a3f2c005e6c
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  computed:{
+
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style>
+/* Add your styles here */
+</style>
