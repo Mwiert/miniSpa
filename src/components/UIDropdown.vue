6206460ca549d1d9bca42fec076d426ebcec78ea
Nazli Gonca <nazligonca@Nazlis-MacBook-Air.local>
2024-07-09 12:04:04 +0300

    commit denemesi

--- src/components/UIDropdown.vue
+++ src/components/UIDropdown.vue
@@ -1,28 +1,103 @@
 <template>
-    <div >
-        denemedir
-    </div>
-  </template>
-  
-  <script>
- 
-  
-  export default {
-    name: 'UIDropdown',
-    components: {
-    },
-    data() {
-    return {
-    }
-  },
-  created() {},
-  mounted() {},
-  computed: {},
-  methods: {}
-  
+<div class="dropdown-wrapper">
+<div class="dropdown-selected-item">
+  {{  selectedItem || 'Please select something' }}
+</div>
+<div class="items-wrapper">
+  <div class="item" v-for="item in items " :key="index">
+    {{ item }}
+  </div>
+</div>
+</div>
+</template>
+
+<script lang="ts">
+
+export default {
+name: 'UIDropdown',
+props: {
+items: { type: Array },
+selectedItem: {  },
+selectedItem: {  },
+placeHolder: {  }
+
+},
+data() {
+return {
+  isOpen: false,
   }
-  </script>
-  
-  <style>
-  /* Add your styles here */
-  </style> 
+      
+},
+created() {},
+mounted() {},
+methods: {
+toggleDropdown() {
+  this.isOpen = !this.isOpen;
+},
+sendItem(items) {
+  this.$emit('sendItem',items)
+}
+},
+computed: {
+airlineName() {
+  return this.items.airlineLabel + " " + this.items.airlineBrand
+}
+}
+}
+
+</script>
+
+<style>
+
+.dropdown-wrapper {
+  padding: 16px;
+  cursor: pointer;
+  max-width: 200px;
+  margin: 0 auto;
+}
+
+.dropdown-selected-item {
+  padding: 16px;
+  border: solid 1px #313131;
+  border-radius: 8px;
+  box-sizing: border-box;
+  margin-bottom: 4px;
+}
+
+.options-wrapper {
+display: none;
+position: absolute;
+background-color: white;
+box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+min-width: 160px;
+z-index: 1;
+border-radius: 5px;
+overflow: hidden;
+}
+.item:hover{
+  background: #c5c5c5;
+}
+.item{
+padding: 16px;
+border: solid 1px #313131;
+box-sizing: border-box;
+}
+.item:last-of-type{
+border-bottom-left-radius: 8px;
+border-bottom-right-radius: 8px;
+}
+.slide-fade-enter-active {
+transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+slide-fade-leave-to {
+transform: translate(-4px) ;
+opacity: 0;
+}
+
+</style>
--- src/pages/SummerLovers.vue
+++ src/pages/SummerLovers.vue
@@ -1,20 +1,33 @@
 <template>
-    <div>
-      <UIDropdown />
-    </div>
-  </template>
-  
+  <div class="summerLovers-c">
+      <UIDropdown :items="item" @sendItem="handlerSelectedItem" />
+  </div>
+</template>
 
-  <script lang="ts">
 
-  import UIDropdown from "../components/UIDropdown.vue";
-  export default {
-    name: 'SummerLovers',
-    components: {
+<script lang="ts">
+import UIDropdown from '../components/UIDropdown.vue'
+export default {
+  name: 'summerLovers',
+  components: {
       UIDropdown
-    },
+  },
+  data() {
+      return {
+          items:
+          [
+        'Türk Hava Yollari','Pegasus','Sun Express'
+      ]
+      }
+  },
+  methods: {
+      handlerSelectedItem(item){
+          this.selectedItem = item     
+         }
   }
-  </script>
-  
-  <style lang="scss" scoped></style>
-  
+}
+
+</script>
+
+<style>
+</style>
