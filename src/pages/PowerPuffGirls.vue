93ba82318956c2a0817576e3b5073fce71c99ee4
Sezen Huddamoğlu <123094585+SezenHuddamoglu@users.noreply.github.com>
2024-07-09 14:22:11 +0300

    disabled button fixed

--- src/pages/PowerPuffGirls.vue
+++ src/pages/PowerPuffGirls.vue
@@ -1,60 +1,105 @@
 <template>
-    <div class="button-c">
-        <UIButton className="flight" text="Flight Button" :icon="flightIcon" :isSpinnerActive="loadingStates.flight"
-            :isDisabled="loadingStates.flight" @click="setLoadingState('flight')" />
-            <UIButton className="flight" :icon="flightIcon" :isSpinnerActive="loadingStates.flight"
-            :isDisabled="loadingStates.flight" @click="setLoadingState('flight')" />
-            <UIButton className="hotel" text="Hotel Button" :isSpinnerActive="loadingStates.hotel"
-            :isDisabled="loadingStates.hotel" @click="setLoadingState('hotel')" />
-        <UIButton className="reisetech-btn" text="Default Button" :isSpinnerActive="loadingStates.default"
-            :isDisabled="loadingStates.default" @click="setLoadingState('default')" />
-        <UIButton className="flight outline" text="Flight Outline Button" :isSpinnerActive="loadingStates.flightOutline"
-            :isDisabled="loadingStates.flightOutline" @click="setLoadingState('flightOutline')" />
-        <UIButton className="hotel outline" text="Hotel Outline Button" :isSpinnerActive="loadingStates.hotelOutline"
-            :isDisabled="loadingStates.hotelOutline" @click="setLoadingState('hotelOutline')" routerUrl="/" />
-        <UIButton className="disabled" text="Disabled Button" :isSpinnerActive="loadingStates.disabledButton"
-            :isDisabled="loadingStates.disabledButton" @click="setLoadingState('disabledButton')" />
-    </div>
+  <div class="button-c">
+    <UIButton
+      className="flight"
+      text="Flight Button"
+      :icon="flightIcon"
+      :isSpinnerActive="loadingStates.flight.isLoading"
+      :isDisabled="loadingStates.flight.isDisabled"
+      @click="setLoadingState('flight')"
+    />
+
+    <UIButton
+      className="flight"
+      :icon="flightIcon"
+      :isSpinnerActive="loadingStates.flight2.isLoading"
+      :isDisabled="loadingStates.flight2.isDisabled"
+      @click="setLoadingState('flight2')"
+    />
+
+    <UIButton
+      className="hotel"
+      text="Hotel Button"
+      :isSpinnerActive="loadingStates.hotel.isLoading"
+      :isDisabled="loadingStates.hotel.isDisabled"
+      @click="setLoadingState('hotel')"
+    />
+
+    <UIButton
+      className="reisetech-btn"
+      text="Default Button"
+      :isSpinnerActive="loadingStates.default.isLoading"
+      :isDisabled="loadingStates.default.isDisabled"
+      @click="setLoadingState('default')"
+    />
+
+    <UIButton
+      className="flight outline"
+      text="Flight Outline Button"
+      :isSpinnerActive="loadingStates.flightOutline.isLoading"
+      :isDisabled="loadingStates.flightOutline.isDisabled"
+      @click="setLoadingState('flightOutline')"
+    />
+
+    <UIButton
+      className="hotel outline"
+      text="Hotel Outline Button"
+      :isSpinnerActive="loadingStates.hotelOutline.isLoading"
+      :isDisabled="loadingStates.hotelOutline.isDisabled"
+      @click="setLoadingState('hotelOutline')"
+      :isRouter="true"
+      routerUrl="/"
+    />
+
+    <UIButton
+      className="disabled"
+      text="Disabled Button"
+      :isSpinnerActive="loadingStates.disabledButton.isLoading"
+      :isDisabled="loadingStates.disabledButton.isDisabled"
+      @click="setLoadingState('disabledButton')"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import UIButton from '../components/UIButton.vue';
-import flightIcon from '../assets/images/flight.png';
+import { defineComponent } from 'vue'
+import UIButton from '../components/UIButton.vue'
+import flightIcon from '../assets/images/flight.png'
 
 export default defineComponent({
-    name: 'PowerPuffGirls',
-    components: {
-        UIButton
-    },
-    data() {
-        return {
-            flightIcon,
-            loadingStates: {
-                flight: false,
-                hotel: false,
-                default: false,
-                flightOutline: false,
-                hotelOutline: false,
-                disabledButton: true
-            }
-        };
-    },
-    methods: {
-        setLoadingState(buttonName: string) {
-            this.loadingStates[buttonName] = true;
-            setTimeout(() => {
-                this.loadingStates[buttonName] = false;
-            }, 2000);
-        }
+  name: 'PowerPuffGirls',
+  components: {
+    UIButton
+  },
+  data() {
+    return {
+      flightIcon,
+      loadingStates: {
+        flight: { isLoading: false, isDisabled: false },
+        flight2: { isLoading: false, isDisabled: false },
+        hotel: { isLoading: false, isDisabled: false },
+        default: { isLoading: false, isDisabled: false },
+        flightOutline: { isLoading: false, isDisabled: false },
+        hotelOutline: { isLoading: false, isDisabled: false },
+        disabledButton: { isLoading: false, isDisabled: true }
+      }
     }
-});
+  },
+  methods: {
+    setLoadingState(buttonName: string) {
+      this.loadingStates[buttonName].isLoading = true
+      setTimeout(() => {
+        this.loadingStates[buttonName].isLoading = false
+      }, 2000)
+    }
+  }
+})
 </script>
 
 <style lang="scss" scoped>
 .button-c {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 </style>
