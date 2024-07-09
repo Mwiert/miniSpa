fee64e669bd3ea9e1c162b82590052a398ed0024
Defne Bulut <d.bulut0907@gmail.com>
2024-07-09 16:53:50 +0300

    Team classes added

--- src/components/UIButton.vue
+++ src/components/UIButton.vue
@@ -1,160 +1,209 @@
-<template> 
+<template>
   <!-- btnIsRouter is checking for button has routing function-->
   <!-- If spinner is not active icon and text will be shown-->
-    <button class="reisetech-btn" :class="`reisetech-btn__${btnClassName}`" :disabled="isDisabled"
-      v-if="!btnIsRouter">
-      <span v-if="icon && !isSpinnerActive" class="icon">
-        <img :src="icon" alt="button icon">
-      </span>
-  
-      <span v-if="isSpinnerActive" class="spinner"></span>
-      <span v-else>{{ btnText }}</span>
-  
-    </button>
-    <router-link :to="routerUrl" class="reisetech-btn" :class="`reisetech-btn__${btnClassName}`" replace="button" v-else>
-      <span v-if="icon" class="icon" :class="btnIcon"></span>
-      <span v-if="isSpinnerActive" class="spinner"></span>
-      <span v-else>{{ btnText }}</span>
-    </router-link>
-  
-  
-  </template>
-  
-  <script lang="ts">
-  export default {
-    name: 'UIButton',
-    props: {
-      // Classes such as hotel and flight are derived from the .reisetech-btn class.
-      // .reisetech-btn class has the default css style, but the appearance of the button changes depending on the incoming flight and hotel class.
-      className: String,
-      // Text is the label of button
-      text: String,
-      // isRouter is checking the button is routing or not
-      isRouter: Boolean,
-      // routeUrl is the routing Url
-      routerUrl: String,
-      Object,
-      // icon is the icons on the button if button has one
-      icon: String,
-      // isSpinner is checking loading animation for button exist or not
-      isSpinnerActive: Boolean,
-      // isDisabled is checking button is active or disabled
-      isDisabled: Boolean
-    },
-    data() {
-      return {
-        btnClassName: this.className,
-        btnText: this.text,
-        btnIcon: this.icon,
-        btnIsRouter: this.isRouter
-      }
-    },
-    methods: {
-     
+  <button class="reisetech-btn" :class="`reisetech-btn__${btnClassName}`" :disabled="isDisabled" v-if="!btnIsRouter">
+    <span v-if="icon && !isSpinnerActive" class="icon">
+      <img :src="icon" alt="button icon">
+    </span>
+
+    <span v-if="isSpinnerActive" class="spinner"></span>
+    <span v-else>{{ btnText }}</span>
+
+  </button>
+  <router-link :to="routerUrl" class="reisetech-btn" :class="`reisetech-btn__${btnClassName}`" replace="button" v-else>
+    <span v-if="icon && !isSpinnerActive" class="icon">
+      <img :src="icon" alt="button icon">
+    </span>
+
+    <span v-if="isSpinnerActive" class="spinner"></span>
+    <span v-else>{{ btnText }}</span>
+  </router-link>
+
+
+</template>
+
+<script lang="ts">
+export default {
+  name: 'UIButton',
+  props: {
+    // Classes such as hotel and flight are derived from the .reisetech-btn class.
+    // .reisetech-btn class has the default css style, but the appearance of the button changes depending on the incoming flight and hotel class.
+    className: String,
+    // Text is the label of button
+    text: String,
+    // isRouter is checking the button is routing or not
+    isRouter: Boolean,
+    // routeUrl is the routing Url
+    routerUrl: String,
+    Object,
+    // icon is the icons on the button if button has one
+    icon: String,
+    // isSpinner is checking loading animation for button exist or not
+    isSpinnerActive: Boolean,
+    // isDisabled is checking button is active or disabled
+    isDisabled: Boolean
+  },
+  data() {
+    return {
+      btnClassName: this.className,
+      btnText: this.text,
+      btnIcon: this.icon,
+      btnIsRouter: this.isRouter
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/css/main.scss';
+
+// router-link takes tag 'a' and its default css should be overwritten
+a.reisetech-btn {
+  padding: 0px;
+}
+
+.reisetech-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 48px;
+  border-radius: 24px;
+  font-size: 16px;
+  background-color: transparent;
+  border: 2px;
+  color: black;
+  cursor: pointer;
+  padding: $padding-medium;
+  text-align: center;
+  border: 2px solid black;
+  position: relative;
+  text-decoration: none;
+
+  &__flight {
+    background-color: $primary-color;
+    border: none;
+    color: white;
+
+    &.outline {
+      background-color: white;
+      color: $primary-color;
+      border: 2px solid $primary-color;
     }
   }
-  </script>
-  
-  <style lang="scss" scoped>
-  @import '@/assets/css/main.scss';
-  // router-link takes tag 'a' and its default css should be overwritten
-  a.reisetech-btn {
-  padding:0px;
+
+  &__hotel {
+    background-color: $secondary-color;
+    border: none;
+    color: white;
+
+    &.outline {
+      background-color: white;
+      color: $secondary-color;
+      border: 2px solid $secondary-color;
+    }
+
   }
-  
-  .reisetech-btn {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 48px;
-    border-radius: 24px;
-    font-size: 16px;
-    background-color: transparent;
-    border: 2px;
-    color: black;
-    cursor: pointer;
-    padding: $padding-medium;
-    text-align: center;
-    border: 2px solid black;
-    position: relative;
-    text-decoration: none;
-    
-    &__flight {
-      background-color: $primary-color;
-      border: none;
-      color: white;
-  
-      &.outline {
-        background-color: white;
-        color: $primary-color;
-        border: 2px solid $primary-color;
-      }
+
+  &__powerpuff-girls {
+    background-color: #98029b;
+    border: none;
+    color: white;
+
+    &.outline {
+      background-color: white;
+      color: $primary-color;
+      border: 2px solid #98029b;
     }
-  
-    &__hotel {
-      background-color: $secondary-color;
-      border: none;
-      color: white;
-  
-      &.outline {
-        background-color: white;
-        color: $secondary-color;
-        border: 2px solid $secondary-color;
-      }
-  
+  }
+
+  &__pink-panthers {
+    background-color: #F39FB1;
+    border: none;
+    color: white;
+
+    &.outline {
+      background-color: white;
+      color: $primary-color;
+      border: 2px solid #F39FB1;
     }
-  
+  }
+
+  &__summer-lovers {
+    background-color: #83d9e5;
+    border: none;
+    color: white;
+
+    &.outline {
+      background-color: white;
+      color: $primary-color;
+      border: 2px solid #83d9e5;
+    }
+  }
+
+  &__time-benders {
+    background-color: #8fbd8d;
+    border: none;
+    color: white;
+
+    &.outline {
+      background-color: white;
+      color: $primary-color;
+      border: 2px solid #8fbd8d;
+    }
+  }
+
+  &:hover {
+    background: $accent-primary-color;
+    border: none;
+    color: white;
+  }
+
+  &__disabled {
+    background: #e0e0e0;
+    border: 2px solid #bdbdbd;
+    color: #9e9e9e;
+    cursor: not-allowed;
+    opacity: 0.6;
+
     &:hover {
-      background: $accent-primary-color;
-      border: none;
-      color: white;
-    }
-  
-    &__disabled {
       background: #e0e0e0;
       border: 2px solid #bdbdbd;
       color: #9e9e9e;
-      cursor: not-allowed;
-      opacity: 0.6;
-  
-      &:hover {
-        background: #e0e0e0;
-        border: 2px solid #bdbdbd;
-        color: #9e9e9e;
-      }
     }
   }
-  
-  .icon {
-    display: inline-block;
-    margin-right: 8px;
-  
-    img {
-      width: 24px;
-      height: 24px;
-    }
-  }
-  
-  .spinner {
-    border: 4px solid rgba(58, 57, 57, 0.3);
-    border-radius: 50%;
-    border-top: 4px solid rgb(219, 219, 219);
+}
+
+.icon {
+  display: inline-block;
+  margin-right: 8px;
+
+  img {
     width: 24px;
     height: 24px;
-    animation: spin 2s linear infinite;
-    position: absolute;
   }
-  
-  @keyframes spin {
-    0% {
-      transform: rotate(0deg);
-    }
-  
-    100% {
-      transform: rotate(360deg);
-    }
+}
+
+.spinner {
+  border: 4px solid rgba(58, 57, 57, 0.3);
+  border-radius: 50%;
+  border-top: 4px solid rgb(219, 219, 219);
+  width: 24px;
+  height: 24px;
+  animation: spin 2s linear infinite;
+  position: absolute;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
   }
-  </style>
-  
-  
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
