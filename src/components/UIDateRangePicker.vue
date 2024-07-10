922e787278391103ea70c773f35620f8788326ab
Emre Topcu <114030199+EmreTopcu04@users.noreply.github.com>
2024-07-10 18:03:36 +0300

    SCSS Fix

---
+++ src/components/UIDateRangePicker.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="ui-date-range-picker-c">   
+        <button @click="toggleCalendar()" class="button">fdsjfdshnsfj</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'UIDateRangePicker',
+    data() {
+        return {
+            closeCalendar: false
+        }
+    },
+    methods: {
+        toggleCalendar(){
+            this.closeCalendar = !this.closeCalendar
+            console.log(this.closeCalendar)
+            this.$emit('closeCalendar', this.closeCalendar)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.ui-date-picker-c {
+    padding: 410px;
+}
+
+</style>
+
+
--- src/pages/TimeBenders.vue
+++ src/pages/TimeBenders.vue
@@ -1,28 +1,37 @@
 <template>
-    <div>
-        <UIDatePicker :yearRange="4" :initialDate="selectedDate" @dateSelected="handleDateSelected" />
+    <div class="time-benders-c">
+        <UIDateRangePicker @closeCalendar="handleCalendar"/>
+        <UIDatePicker :yearRange="4" :initialDate="selectedDate" @dateSelected="handleDateSelected" v-show="closeCalendar"/>
     </div>
 </template>
 
 <script lang="ts">
 import UIDatePicker from '../components/UIDatePicker.vue';
+import UIDateRangePicker from '../components/UIDateRangePicker.vue';
 import dayjs from 'dayjs';
 export default {
     
     name: 'TimeBenders',
     components: {
         UIDatePicker,
+        UIDateRangePicker,
     },
     data() {
         return {
             selectedDate: dayjs().format('YYYY-MM-DD'),
+            closeCalendar: true
+           
         }
     },
     methods: {
-
+        
         handleDateSelected(firstDate: string) {
             this.selectedDate = firstDate;           
             console.log("Selected Date: ", firstDate)
+        },
+        handleCalendar(isCalenderClosed: Boolean){
+            this.closeCalendar = isCalenderClosed
+            console.log("Calendar Closed: ", isCalenderClosed)
         }
     }
 
@@ -33,5 +42,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    
+.time-benders-c{   
+
+
+}
+
+
+
 </style>
+
