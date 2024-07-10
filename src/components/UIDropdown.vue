da38a40909b8cbbb73eb62e189c7d3a342016fff
ismailbas <20180808033@ogr.akdeniz.edu.tr>
2024-07-10 12:27:19 +0300

    made optimizations

--- src/components/UIDropdown.vue
+++ src/components/UIDropdown.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="dropdown-c">
-    <div class="dropdown">
-      <button
-        @click="toggleDropdown"
-        :class="{ 'dropdown-button-active': isOpen }"
-        class="dropdown-button"
-      >
-        {{ selectedItem || placeHolder }}
-        <span class="arrow" :class="{ 'arrow-up': isOpen }"></span>
-      </button>
-      <div v-if="isOpen" class="dropdown-menu">
+  <div class="ui-dropdown-c">
+    <button
+      @click="toggleDropdown"
+      :class="{ 'dropdown-button-active': isOpen }"
+      class="dropdown-button"
+    >
+      {{ selectedItem || placeHolder }}
+      <span class="arrow" :class="{ 'arrow-up': isOpen }"></span>
+    </button>
+    <div v-if="isOpen" class="dropdown-menu">
+      <div class="search-container">
         <input
           v-if="searchable"
           type="text"
@@ -17,14 +17,15 @@
           placeholder="Search..."
           class="dropdown-search"
         />
-        <div
-          v-for="(item, index) in items"
-          :key="index"
-          @click="selectItem(item)"
-          class="dropdown-item"
-        >
-          {{ item }}
-        </div>
+        <span v-if="searchQuery" class="clear-search" @click="clearSearch">×</span>
+      </div>
+      <div
+        v-for="(item, index) in filteredItems"
+        :key="index"
+        @click="selectItem(item)"
+        class="dropdown-item"
+      >
+        {{ item }}
       </div>
     </div>
   </div>
@@ -48,7 +49,7 @@ export default {
     },
     placeHolder: {
       type: String,
-      default: 'Select'
+      default: 'Select an Item'
     },
     searchable: {
       type: Boolean,
@@ -59,11 +60,11 @@ export default {
     }
   },
   computed: {
-    // filteredItems(): string[] {
-    //   return this.items.filter((item: string) =>
-    //     item.toLowerCase().includes(this.searchQuery.toLowerCase())
-    //   )
-    // }
+    filteredItems(): string[] {
+      return this.items.filter((item: string) =>
+        item.toLowerCase().includes(this.searchQuery.toLowerCase())
+      )
+    }
   },
   methods: {
     toggleDropdown() {
@@ -74,6 +75,11 @@ export default {
       this.isOpen = false
       this.$emit('update:selectedItem', item)
     },
+
+    clearSearch() {
+      this.searchQuery = ''
+    },
+
     handleClickOutside(event: MouseEvent) {
       const target = event.target as HTMLElement
       if (!this.$el.contains(target)) {
@@ -91,107 +97,108 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dropdown {
+.ui-dropdown-c {
   position: relative;
   display: inline-block;
   margin: 10px;
-}
 
-.dropdown-button {
-  padding: 15px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  .dropdown-button {
+    padding: 15px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-  &-active {
-    border: 1px solid #000;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  }
-
-  .arrow {
-    margin-left: 15px;
-    border: solid black;
-    border-width: 0 1px 1px 0;
-    display: inline-block;
-    padding: 5px;
-    transform: rotate(45deg);
-
-    &-up {
-      transform: rotate(-135deg);
-    }
-  }
-}
-
-.dropdown-menu {
-  position: absolute;
-  top: 100%;
-  left: 0;
-  right: 0;
-  background-color: #fff;
-  border: 2px solid #ccc;
-  border-radius: 8px;
-  max-height: 300px;
-  overflow-y: auto;
-  z-index: 1000;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-
-  .search-container {
-    position: relative;
-
-    .dropdown-search {
-      width: 90%;
-      padding: 10px;
-      box-sizing: border-box;
-      margin: 7px;
-      border-radius: 10px;
-      border: 2px solid #ccc;
-      outline: none;
+    &-active {
+      border: 1px solid #000;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     }
 
-    .clear-search {
-      position: absolute;
-      right: 12px;
-      top: 13px;
-      transform: translateY(-50%, -50%);
-      cursor: pointer;
-      font-size: 25px;
-      color: #ccc;
-      width: 25px;
-      height: 25px;
-      background-color: #f0f0f0;
-      border-radius: 50%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
+    .arrow {
+      margin-left: 15px;
+      border: solid black;
+      border-width: 0 1px 1px 0;
+      display: inline-block;
+      padding: 5px;
+      transform: rotate(45deg);
 
-      &:hover {
-        color: #000;
+      &-up {
+        transform: rotate(-135deg);
       }
     }
   }
 
-  .dropdown-item {
-    padding: 10px;
-    cursor: pointer;
-    border-bottom: 1px solid #f0f0f0;
+  .dropdown-menu {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    background-color: #fff;
+    border: 2px solid #ccc;
+    border-radius: 8px;
+    max-height: calc(7 * 50px);
+    overflow-x: hidden;
+    overflow-y: auto;
+    z-index: 1000;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
-    &:hover {
-      background-color: #f3f3f3;
+    .search-container {
+      position: relative;
+
+      .dropdown-search {
+        width: 90%;
+        padding: 10px;
+        box-sizing: border-box;
+        margin: 7px;
+        border-radius: 10px;
+        border: 2px solid #ccc;
+        outline: none;
+      }
+
+      .clear-search {
+        position: absolute;
+        right: 12px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+        font-size: 25px;
+        color: #ccc;
+        width: 25px;
+        height: 25px;
+        background-color: #f0f0f0;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+          color: #000;
+        }
+      }
+    }
+
+    .dropdown-item {
+      padding: 10px;
+      cursor: pointer;
+      border-bottom: 1px solid #f0f0f0;
+
+      &:hover {
+        background-color: #f3f3f3;
+      }
     }
   }
-}
 
-.dropdown-search {
-  width: 90%;
-  padding: 10px;
-  box-sizing: border-box;
-  margin: 7px;
-  border-radius: 10px;
-  border: 2px solid #ccc;
-  outline: none;
+  .dropdown-search {
+    width: 90%;
+    padding: 10px;
+    box-sizing: border-box;
+    margin: 7px;
+    border-radius: 10px;
+    border: 2px solid #ccc;
+    outline: none;
+  }
 }
 </style>
