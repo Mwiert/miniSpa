1aaca52d7c1ef6e393c5f2d0b082264977b4433d
Emre Topcu <114030199+EmreTopcu04@users.noreply.github.com>
2024-07-17 12:21:48 +0300

    Fix Bugs and Implement New Features

--- src/components/UIDropdown.vue
+++ src/components/UIDropdown.vue
@@ -1,57 +1,56 @@
 <template>
   <div class="ui-dropdown-c">
     <div class="ui-dropdown-c-wrapper">
-    <label class="label">{{ label }}</label>
-    <button @click="toggleDropdown" class="ui-dropdown-button" :class="{ active: isOpen }">
-      <span
-        :class="{ 'placeholder-text-active': !selectedItem[displayField] }"
-        class="placeholder-text"
-      >
-        {{ selectedItem[displayField] || placeHolder }}
-      </span>
-
-      <SvgIcon class="arrow" :class="{ up: isOpen }" :name="'arrow-down'" :size="'s'" />
-    </button>
-    <div v-if="isOpen" class="ui-dropdown-menu" :style="{ fontSize: fontSize + 'px' }">
-      <div class="search-container">
-        <div v-if="searchable" class="search-content-wrapper">
-          <input
-            v-if="searchable"
-            type="text"
-            v-model="searchQuery"
-            placeholder="Search..."
-            class="ui-dropdown-search"
-            @input="filteredItems()"
-          />
-          <span class="clear-search">
-            <SvgIcon
-              v-if="searchQuery"
-              @click="clearSearch"
-              class="clear-search-img"
-              :name="'x'"
-              :size="'s'"
-            />
-          </span>
-        </div>
-      </div>
-      <div
-        class="ui-dropdown-content"
-        :style="{ fontSize: fontSize + 'px', maxHeight: dropdownListMaxHeight }"
-      >
-        <div
-          v-for="(item, index) in filteredItems()"
-          :key="index"
-          class="ui-dropdown-item"
-          @click="selectItem(item)"
-          :class="{ selected: isSelected(item) }"
+      <label class="label">{{ label }}</label>
+      <button @click="toggleDropdown" class="ui-dropdown-button" :class="{ active: isOpen }">
+        <span
+          :class="{ 'placeholder-text-active': !selectedItem[displayField] }"
+          class="placeholder-text"
         >
-          <!-- <img v-if="item[urlField]" :src="item[urlField]" alt="" class="dropdown-item-img" /> -->
-          <span>{{ item[displayField] }}</span>
+          {{ selectedItem[displayField] || placeHolder }}
+        </span>
+
+        <SvgIcon class="arrow" :class="{ up: isOpen }" :name="'arrow-down'" :size="'s'" />
+      </button>
+      <div v-if="isOpen" class="ui-dropdown-menu" :style="{ fontSize: fontSize + 'px' }">
+        <div class="search-container">
+          <div v-if="searchable" class="search-content-wrapper">
+            <input
+              v-if="searchable"
+              type="text"
+              v-model="searchQuery"
+              placeholder="Search..."
+              class="ui-dropdown-search"
+            />
+            <span class="clear-search">
+              <SvgIcon
+                v-if="searchQuery"
+                @click.stop="clearSearch"
+                class="clear-search-img"
+                :name="'x'"
+                :size="'s'"
+              />
+            </span>
+          </div>
+        </div>
+        <div
+          class="ui-dropdown-content"
+          :style="{ fontSize: fontSize + 'px', maxHeight: dropdownListMaxHeight }"
+        >
+          <div
+            v-for="(item, index) in filteredItems()"
+            :key="index"
+            class="ui-dropdown-item"
+            @click="selectItem(item)"
+            :class="{ selected: isSelected(item) }"
+          >
+            <!-- <img v-if="item[urlField]" :src="item[urlField]" alt="" class="dropdown-item-img" /> -->
+            <span>{{ item[displayField] }}</span>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -115,7 +114,8 @@ export default {
       isOpen: false, // checks that if drowdown is open or not.
       searchQuery: '', // when the user input text, it comes to the searchQuery.
       selectedItem: this.modelValue, // represents the currently selected item.
-      dropdownItems: this.items
+      dropdownItems: this.items,
+      scrollPosition: 0
     }
   },
   computed: {
@@ -140,20 +140,34 @@ export default {
       return this.selectedItem === item
     },
     selectItem(item) {
-      this.selectedItem = item ? item : {}
-      this.$emit('update:modelValue', item)
+      if (this.selectedItem === item) {
+        this.selectedItem = {}
+        this.$emit('update:modelValue', {})
+      } else {
+        this.selectedItem = item
+        this.$emit('update:modelValue', item)
+      }
+      this.scrollPosition = this.$el.querySelector(".ui-dropdown-content").scrollTop
       this.isOpen = false
       this.dropdownItems = this.items
     },
     toggleDropdown() {
-      //opens and closes the dropdown
+      //Opens and closes the dropdown
       this.isOpen = !this.isOpen
+      //If dropdown is open we are getting the scrollTop location of the content 
+      if(this.isOpen){
+        this.$nextTick(() => {
+          const dropDownContent = this.$el.querySelector(".ui-dropdown-content")
+          dropDownContent.scrollTop = this.scrollPosition
+        }) 
+      }
+
       this.clearSearch()
+      
     },
-    clearSearch(event: Event) {
+    clearSearch() {
       // clears the searchQuery
       this.searchQuery = ''
-      event.stopPropagation() // Prevent dropdown from closing
     },
     handleClickOutside(event: MouseEvent) {
       // when clicked out of the dropdown, dropdownMenu closes.
@@ -161,7 +175,8 @@ export default {
       if (!this.$el.contains(target)) {
         this.isOpen = false
       }
-    }
+    },
+    
   },
   mounted() {
     document.addEventListener('click', this.handleClickOutside) // When is clicked , event occurs anywhere on the document. this.handleClickOutside is invoked.
@@ -174,147 +189,150 @@ export default {
     value(newVal) {
       this.selectedItem = newVal
     }
-  }
+  },
+
+  
 }
 </script>
 
 <style lang="scss" scoped>
 .ui-dropdown-c {
   display: inline-block;
+  justify-content: space-around;
   width: fit-content;
-  .ui-dropdown-c-wrapper{
+  padding: 10px;
+  .ui-dropdown-c-wrapper {
     position: relative;
-  display: flex;
-  flex-direction: column;
-  width: fit-content;
-  .label {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 15px;
-    font-size: 16px;
-    font-weight: 70;
-    color: #333;
-    text-align: left; //Left aligned
-    align-self: flex-start; // Align label to the start
-  }
-
-  .ui-dropdown-button {
-    max-width: fit-content;
-    padding: 8px; // smaller padding
-    background-color: #fff;
-    border: 1px solid #ccc;
-    border-radius: 10px; // smaller border-radius
-    cursor: pointer;
-    justify-content: space-between;
-    align-items: center;
-    display: flex;
-
-    &.active {
-      border: 1px solid #60acfe;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
-
-    .placeholder-text {
+    flex-direction: column;
+    width: fit-content;
+    .label {
       display: flex;
-      font-size: 15px;
-      // font-weight: bold;
-
-      &:active {
-        color: grey;
-        font-weight: normal;
-      }
-    }
-    .arrow {
-      padding: 5px;
-
-      &.up {
-        transform: rotate(180deg);
-      }
-    }
-  }
-
-  .ui-dropdown-menu {
-    position: absolute;
-    width: 10rem;
-    top: 100%;
-    left: 0;
-    right: 0;
-    margin-top: 0.2rem;
-    padding-bottom: 1rem;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    border-radius: 12px;
-    overflow-x: hidden;
-    overflow-y: auto;
-    z-index: 1000;
-    box-shadow: 8px 10px 8px rgba(0, 0, 0, 0.1);
-    .search-container {
-      background-color: #fff;
+      justify-content: center;
       align-items: center;
-      min-width: fit-content;
+      margin-top: 15px;
+      font-size: 16px;
+      font-weight: 70;
+      color: #333;
+      text-align: left; //Left aligned
+      align-self: flex-start; // Align label to the start
+    }
 
-      .search-content-wrapper {
+    .ui-dropdown-button {
+      max-width: fit-content;
+      padding: 8px; // smaller padding
+      background-color: #fff;
+      border: 1px solid #ccc;
+      border-radius: 10px; // smaller border-radius
+      cursor: pointer;
+      justify-content: space-between;
+      align-items: center;
+      display: flex;
+
+      &.active {
+        border: 1px solid #60acfe;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      }
+
+      .placeholder-text {
         display: flex;
-        flex-direction: row;
-        position: relative;
-        box-sizing: border-box;
-        margin: 10px;
-        border-radius: 10px;
-        border: 1px solid #ccc;
-        align-items: center;
+        font-size: 15px;
+        // font-weight: bold;
 
-        .ui-dropdown-search {
-          max-width: fit-content;
-          width: 100%;
-          padding: 10px;
-          border-radius: 10px;
-          border: none;
-          &:focus {
-            outline: none;
-          }
+        &:active {
+          color: grey;
+          font-weight: normal;
         }
+      }
+      .arrow {
+        padding: 5px;
 
-        .clear-search {
+        &.up {
+          transform: rotate(180deg);
+        }
+      }
+    }
+
+    .ui-dropdown-menu {
+      position: absolute;
+      width: 10rem;
+      top: 100%;
+      left: 0;
+      right: 0;
+      margin-top: 0.2rem;
+      padding-bottom: 1rem;
+      background-color: #fff;
+      border: 1px solid #ccc;
+      border-radius: 12px;
+      overflow-x: hidden;
+      overflow-y: auto;
+      z-index: 1000;
+      box-shadow: 8px 10px 8px rgba(0, 0, 0, 0.1);
+      .search-container {
+        background-color: #fff;
+        align-items: center;
+        min-width: fit-content;
+
+        .search-content-wrapper {
           display: flex;
+          flex-direction: row;
+          position: relative;
+          box-sizing: border-box;
+          margin: 10px;
+          border-radius: 10px;
+          border: 1px solid #ccc;
           align-items: center;
-          padding-right: 1.25rem;
-          height: 100%;
-          width: 15%;
 
-          .clear-search-img {
-            position: absolute;
-            cursor: pointer;
-            width: 1rem;
-            height: 1rem;
-            &:hover {
-              filter: opacity(0.5);
+          .ui-dropdown-search {
+            max-width: fit-content;
+            width: 100%;
+            padding: 10px;
+            border-radius: 10px;
+            border: none;
+            &:focus {
+              outline: none;
+            }
+          }
+
+          .clear-search {
+            display: flex;
+            align-items: center;
+            padding-right: 1.25rem;
+            height: 100%;
+            width: 15%;
+
+            .clear-search-img {
+              position: absolute;
+              cursor: pointer;
+              width: 1rem;
+              height: 1rem;
+              &:hover {
+                filter: opacity(0.5);
+              }
             }
           }
         }
       }
-    }
 
-    .ui-dropdown-content {
-      overflow-y:auto;
+      .ui-dropdown-content {
+        overflow-y: auto;
 
+        .ui-dropdown-item {
+          display: flex;
+          align-items: center;
+          padding: 8px;
+          cursor: pointer;
+          transition: background-color 0.3s;
 
-      .ui-dropdown-item {
-        display: flex;
-        align-items: center;
-        padding: 8px;
-        cursor: pointer;
-        transition: background-color 0.3s;
-
-        &:hover {
-          background-color: #f3f3f3;
-        }
-        &.selected {
-          font-weight: bold;
+          &:hover {
+            background-color: #f3f3f3;
+          }
+          &.selected {
+            font-weight: bold;
+          }
         }
       }
     }
   }
-}
 }
 </style>
