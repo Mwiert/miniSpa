8431cafdc94d9aef773cc153d433e862be46a48f
HakanZavrak <hakanzavrak2000@gmail.com>
2024-07-26 15:33:40 +0300

    Hotfix

--- src/components/UIDatePicker.vue
+++ src/components/UIDatePicker.vue
@@ -308,6 +308,7 @@ export default {
     flex-direction: column;
     align-items: center;
     position: relative;
+    margin-top: 1.5rem; 
     //This is the triangle for the calendar
     &::before {
       content: '';
