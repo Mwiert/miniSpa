4339df7ccbbe27f6bfb18baedbf2022fb0a8a142
emreugdul9 <20190808029@ogr.akdeniz.edu.tr>
2024-07-30 12:07:37 +0300

    MenuDropdown init

---
+++ src/components/Dropdown/MenuDropdown.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="menu-dropdown-c">
+        <slot :childClick="childClick"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MenuDropdown",
+        methods: {
+            childClick() {
+                window.alert('child click')
+            }
+        },
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
