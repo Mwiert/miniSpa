a1b0f0f7a5b14a3c04ec43c82702631b8da7309a
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-08-01 16:22:53 +0300

    fixed above opening triangle

--- src/components/Dropdown/MenuDropdown.vue
+++ src/components/Dropdown/MenuDropdown.vue
@@ -224,6 +224,18 @@ export default {
         &::before {
           display: none;
         }
+        &::after {
+          content: '';
+          position: relative;
+          top: 6px;
+          left: 30px;
+          width: 0;
+          height: 0;
+          border-left: 10px solid transparent;
+          border-right: 10px solid transparent;
+          border-top: 10px solid white;
+          z-index: 999;
+        }
         //after çentiği gelecek
       }
       &.direct-right-wrapper {
