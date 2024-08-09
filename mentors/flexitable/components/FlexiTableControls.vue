5a7aea85fa4bc596e128fbc26d1f21e2166dafbc
Defne Bulut <d.bulut0907@gmail.com>
2024-08-09 13:53:27 +0300

    css upğdates

--- mentors/flexitable/components/FlexiTableControls.vue
+++ mentors/flexitable/components/FlexiTableControls.vue
@@ -5,19 +5,13 @@
       <div class="spinner"></div>
     </div>
     <div class="ftc-select-wrapper">
-      <UIEnumDropdown
-        v-model="flexi.options.selected"
-        :enumObj="updatedPageOrder"
-        :dataSize="flexi.options.UIDropdownOrderProp.dataSize"
-        :fontSize="flexi.options.UIDropdownOrderProp.fontSize"
+      <UIEnumDropdown v-model="flexi.options.selected" :enumObj="updatedPageOrder"
+        :dataSize="flexi.options.UIDropdownOrderProp.dataSize" :fontSize="flexi.options.UIDropdownOrderProp.fontSize"
         :showAll="flexi.options.UIDropdownOrderProp.showAll" />
       <!--Custom Dropdown-->
 
       <div class="export-buttons">
-        <div
-          class="excel-selector"
-          @mouseover="openComponent = true"
-          @mouseleave="openComponent = false">
+        <div class="excel-selector" @mouseover="openComponent = true" @mouseleave="openComponent = false">
           <SvgIcon name="excel" size="xs" class="excel-button" />
           <div class="excel-wrapper" v-if="openComponent">
             <div class="excel-text" @click="downloadExcel()">Download Excel</div>
@@ -25,23 +19,15 @@
           </div>
         </div>
         <SvgIcon name="pdf" size="xs" class="pdf-button" @click="downloadPdf()"></SvgIcon>
-        <SvgIcon
-          name="print"
-          size="xs"
-          class="print-button"
-          @click="triggerExportPrint()"></SvgIcon>
+        <SvgIcon name="print" size="xs" class="print-button" @click="triggerExportPrint()"></SvgIcon>
         <div class="dropdown">
           <div class="dropdown-icon" @click="Toggle()">
             <SvgIcon class="dropdown-icon" name="columns" size="xs" />
           </div>
 
           <div class="multi" v-if="flexi.options.show">
-            <div
-              class="option"
-              :class="col.status === false ? 'notselected' : 'selected'"
-              v-for="(col, index) in flexi.columns"
-              :key="index"
-              @click="selectHidden(index)">
+            <div class="option" :class="col.status === false ? 'notselected' : 'selected'"
+              v-for="(col, index) in flexi.columns" :key="index" @click="selectHidden(index)">
               <div class="option-text" :class="col.status === false ? 'notselected' : 'selected'">
                 {{ col.name }}
               </div>
@@ -65,26 +51,17 @@
     <!-- Search Table -->
     <div class="ftc-right-side-wrapper">
       <div class="mark-sign-wrapper">
-        <FlexiTableActionArea
-          :dropdowns="this.flexi.options.dropdowns"
-          :buttons="this.flexi.options.buttons" />
+        <FlexiTableActionArea :dropdowns="this.flexi.options.dropdowns" :buttons="this.flexi.options.buttons" />
       </div>
 
       <div class="ftc-search-wrapper" v-if="!flexi.options.hideSearch">
         <SvgIcon :name="'search'" size="s" class="search" />
-        <input
-          type="text"
-          v-model="flexi.options.searchWord"
-          @input="
-            debounce(() => {
-              state.filterText = flexi.options.searchWord
-            })
+        <input type="text" v-model="flexi.options.searchWord" @input="
+          debounce(() => {
+            state.filterText = flexi.options.searchWord
+          })
           " />
-        <button
-          type="button"
-          class="clear-button"
-          @click="clearSearch"
-          v-if="flexi.options.searchWord">
+        <button type="button" class="clear-button" @click="clearSearch" v-if="flexi.options.searchWord">
           <SvgIcon :name="'x'" size="s" class="clear" />
         </button>
       </div>
@@ -370,7 +347,7 @@ export default {
     // hide selected column
     selectHidden(index) {
       this.flexi.columns.length - this.flexi.options.hiddenColumns.length >
-      this.flexi.options.minVisibleColumns
+        this.flexi.options.minVisibleColumns
         ? (this.flexi.columns[index].status = !this.flexi.columns[index].status)
         : (this.flexi.columns[index].status = true)
     },
@@ -413,6 +390,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  flex-wrap: wrap;
 
   @keyframes spin {
     0% {
